e735702a0f1ab5f66a8df8b24ae6377dc67c1ca6
Brayden Wilmoth <brayden.wilmoth@gmail.com>
2012-07-18 00:16:18 -0400

    Optimizations
    Removed several unnecessary lines of code, setters/getters, and
    properties defined in the header file.

--- UICheckbox.h
+++ UICheckbox.h
@@ -1,11 +1,13 @@
 //
 //    UICheckbox.h
 //
-//    Author: Brayden Wilmoth
-//    Edited: 07/17/2012
+//    Author:    Brayden Wilmoth
+//    Co-Author: Jordan Perry
+//    Edited:    07/17/2012
 //
 //    Copyright (c) 2012 Brayden Wilmoth.  All rights reserved.
 //    http://www.github.com/brayden/
+//    http://www.github.com/jordanperry/
 //
 
 #import <UIKit/UIKit.h>
@@ -13,8 +15,5 @@
 @interface UICheckbox : UIControl
 
 @property(nonatomic, assign)BOOL isChecked;
-
-@property(nonatomic, weak)UIImage *uncheckedImage;
-@property(nonatomic, weak)UIImage *checkedImage;
 
 @end
